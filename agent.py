fix softmax axis

agent.py
@@ -15,7 +15,7 @@ class NaiveAgent():
 
     def get_action(self, state):
         i, xi = (state[0] * self.n - 0.5).long(), state[1].long()
-        probs = F.softmax(self.theta[i, xi], dim=0)
+        probs = F.softmax(self.theta[i, xi], dim=-1)
 
         action = probs.multinomial(1)
         log_prob = probs.gather(1, action).log().view(-1,)
@@ -24,6 +24,8 @@ class NaiveAgent():
         return action.view(-1,), log_prob, entropy
     
     def update_param(self, rewards, log_probs, entropies):
+        #print(rewards, log_probs)
+
         rewards.reverse()
         log_probs.reverse()
         
@@ -37,6 +39,8 @@ class NaiveAgent():
 
         grads = autograd.grad(outputs=loss, inputs=self.theta, allow_unused=True)
         self.theta = self.theta - self.lr * grads[0]
+
+        #print(loss, grads[0])
 
         return rewards[-1].mean().detach().cpu(), loss.detach().cpu()
 

env.py
@@ -12,7 +12,7 @@ class CSPEnv():
         if reset_perm:
             self.v = torch.zeros((self.bs, self.n), device="cuda")
             for i in range(self.bs):
-                self.v[i] = torch.randperm(self.n)
+                self.v[i] = torch.randperm(self.n, device="cuda")
         self.premax = torch.zeros((self.bs,), device="cuda")
         self.active = torch.ones((self.bs,), device="cuda")
     

train.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     shutil.copy("train.py", os.path.join(logdir, "code"))
 
     env = CSPEnv(args.n, args.batch_size)
-    agent = NaiveAgent(args.n)
+    agent = NaiveAgent(args.n, args.lr)
 
     running_reward, running_loss = 0, 0
 
@@ -79,3 +79,5 @@ if __name__ == "__main__":
             if (episode + 1) % args.save_episode == 0:
                 savepath = os.path.join(logdir, "models/%d.pt" % (episode))
                 torch.save(agent, savepath)
+
+    #env.print_v()